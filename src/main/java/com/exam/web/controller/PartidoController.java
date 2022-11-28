0e8c9e480a21e4f4b466a54e37271023858d8712
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-28 12:16:34 -0500

    Intentar resolver los dos puntos restantes

--- src/main/java/com/exam/web/controller/PartidoController.java
+++ src/main/java/com/exam/web/controller/PartidoController.java
@@ -5,10 +5,12 @@ import com.exam.web.repository.PartidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -21,5 +23,16 @@ public class PartidoController {
     @GetMapping
     public List<Partido> getPartidoAll() {
         return partidoRepo.findAll();
+    }
+
+    @GetMapping("/{id}/resultados")
+    public Partido getPartidobyId(@PathVariable Integer id) {
+        Optional<Partido> partido = partidoRepo.findById(id);
+
+        if (partido.isPresent()) {
+            return partido.get();
+        }
+
+        return null;
     }
 }
