b81a76d5f6933a01ef12520bf4e3ee9ac9ada8e9
BrayanGuerreroXD <bg7047653@gmail.com>
2022-11-28 11:44:51 -0500

    Realizar punto registrar resultado

--- src/main/java/com/exam/web/controller/ResultadoController.java
+++ src/main/java/com/exam/web/controller/ResultadoController.java
@@ -5,6 +5,7 @@ import com.exam.web.repository.ResultadoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,12 @@ public class ResultadoController {
     @GetMapping
     public List<Resultado> getResultadoAll() {
         return resultadoRepo.findAll();
+    }
+
+    @PostMapping
+    public Resultado postResultado(@RequestBody Resultado resultado) {
+        resultadoRepo.save(resultado);
+        return resultado;
     }
 
     @PutMapping("/{id}")
